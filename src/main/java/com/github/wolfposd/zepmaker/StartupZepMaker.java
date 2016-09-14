3a424e9515d82fcc8893fbaa8d31b15d94aae4e3
wolfposd <posdorfer@informatik.uni-hamburg.de>
2016-09-14 10:41:03 +0200

    changing LAF to system-LAF

--- src/main/java/com/github/wolfposd/zepmaker/StartupZepMaker.java
+++ src/main/java/com/github/wolfposd/zepmaker/StartupZepMaker.java
@@ -23,9 +23,19 @@
  */
 package com.github.wolfposd.zepmaker;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class StartupZepMaker {
 
     public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
         new ZepController();
     }
 
